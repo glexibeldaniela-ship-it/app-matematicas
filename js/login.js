c0fb57ef9cc5da0439deaebd8f7e4b397396bd8a
glexibeldaniela-ship-it <glexibeldaniela@gmail.com>
2026-02-21 22:42:24 -0400

    Actualizar login.js

--- js/login.js
+++ js/login.js
@@ -1,4 +1,3 @@
-
 import { auth, db } from "./firebase-config.js";
 import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
@@ -23,7 +22,7 @@ btn.addEventListener("click", async () => {
       return;
     }
 
-    const rol = docSnap.data().rol;
+    const rol = docSnap.data().rol.toLowerCase().trim();
 
     if (rol === "admin") {
       window.location.href = "admin.html";
