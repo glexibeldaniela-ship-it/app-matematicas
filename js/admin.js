7488dec7225bd9f77c65532fa252cfe0ab468975
glexibeldaniela-ship-it <glexibeldaniela@gmail.com>
2026-02-23 10:44:58 -0400

    Actualizar admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,30 @@
-u
+import { auth, db } from "./firebase-config.js";
+import { signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const userInfo = document.getElementById("user-info");
+const btnLogout = document.getElementById("btn-logout");
+
+// 1. Verificar que realmente eres tú
+onAuthStateChanged(auth, async (user) => {
+    if (user) {
+        const docRef = doc(db, "usuarios", user.uid);
+        const docSnap = await getDoc(docRef);
+        
+        if (docSnap.exists() && docSnap.data().rol === "admin") {
+            userInfo.innerText = "Conectada como: " + docSnap.data().nombre;
+        } else {
+            // Si alguien intenta entrar de colado, lo sacamos
+            window.location.href = "../index.html";
+        }
+    } else {
+        window.location.href = "../index.html";
+    }
+});
+
+// 2. Botón para salir
+btnLogout.addEventListener("click", () => {
+    signOut(auth).then(() => {
+        window.location.href = "../index.html";
+    });
+});
