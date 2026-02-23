0bbc9cd30665a862a72e485ab8653865a9d95022
glexibeldaniela-ship-it <glexibeldaniela@gmail.com>
2026-02-23 01:12:47 -0400

    Actualizar firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,6 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+// Tu configuración (La que me pasaste)
 const firebaseConfig = {
   apiKey: "AIzaSyCqSVoNR5y3kXb-Aihf1UwmvTwo1T0xm_8",
   authDomain: "mateedupro2026-897fa.firebaseapp.com",
@@ -11,6 +12,9 @@ const firebaseConfig = {
   appId: "1:242144964491:web:04eb900f523a91ef66498e"
 };
 
+// Inicializamos Firebase
 const app = initializeApp(firebaseConfig);
+
+// ✅ EXPORTAMOS LAS HERRAMIENTAS (Sin esto, el login no funciona)
 export const auth = getAuth(app);
 export const db = getFirestore(app);
