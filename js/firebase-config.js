88df2861058d0facb17d27f494d41fc736504e40
glexibeldaniela-ship-it <glexibeldaniela@gmail.com>
2026-02-23 10:12:22 -0400

    Actualizar firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,19 @@
-y
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCqSVoNR5y3kXb-Aihf1UwmvTwo1T0xm_8",
+  authDomain: "mateedupro2026-897fa.firebaseapp.com",
+  projectId: "mateedupro2026-897fa",
+  storageBucket: "mateedupro2026-897fa.firebasestorage.app",
+  messagingSenderId: "242144964491",
+  appId: "1:242144964491:web:04eb900f523a91ef66498e"
+};
+
+// Inicializamos Firebase
+const app = initializeApp(firebaseConfig);
+
+// Exportamos las herramientas para que los demás archivos las usen
+export const auth = getAuth(app);
+export const db = getFirestore(app);
