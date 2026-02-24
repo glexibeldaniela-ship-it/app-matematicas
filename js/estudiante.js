28aa35c2a1317237610b12d6c4fc9dd5e18c85fe
glexibeldaniela-ship-it <glexibeldaniela@gmail.com>
2026-02-23 22:47:53 -0400

    Actualizar estudiante.js

--- js/estudiante.js
+++ js/estudiante.js
@@ -1,1 +1,32 @@
-u
+import { auth, db } from "./firebase-config.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+onAuthStateChanged(auth, async (user) => {
+
+  if (!user) {
+    window.location.href = "../index.html";
+    return;
+  }
+
+  try {
+    const docRef = doc(db, "usuarios", user.uid);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      const data = docSnap.data();
+
+      if (data.rol !== "estudiante") {
+        alert("Acceso solo para estudiantes");
+        window.location.href = "../index.html";
+      }
+
+    } else {
+      window.location.href = "../index.html";
+    }
+
+  } catch (error) {
+    window.location.href = "../index.html";
+  }
+
+});
