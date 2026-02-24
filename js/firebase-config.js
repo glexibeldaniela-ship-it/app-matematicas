359338cb960dd3b3d249f480b663209fb06c5992
glexibeldaniela-ship-it <glexibeldaniela@gmail.com>
2026-02-23 21:51:36 -0400

    Actualizar firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -16,4 +16,4 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 
-console.log("🔥 Firebase conectado correctamente");
+console.log("🔥 Firebase conectado correctamente"); 
