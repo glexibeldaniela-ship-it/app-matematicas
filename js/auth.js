b51112e59786edd2e045a553ac7338a96d3564c8
glexibeldaniela-ship-it <glexibeldaniela@gmail.com>
2026-02-23 21:53:31 -0400

    Actualizar auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,25 +1,10 @@
 import { auth } from "./firebase-config.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import { db } from "./firebase-config.js";
 
-onAuthStateChanged(auth, async (user) => {
+onAuthStateChanged(auth, (user) => {
   if (user) {
-    const docRef = doc(db, "usuarios", user.uid);
-    const docSnap = await getDoc(docRef);
-
-    if (docSnap.exists()) {
-      const rol = docSnap.data().rol;
-
-      if (rol === "admin") {
-        window.location.href = "administrador/dashboard.html";
-      } 
-      else if (rol === "profesor") {
-        window.location.href = "profesor/panel.html";
-      } 
-      else if (rol === "estudiante") {
-        window.location.href = "estudiantes/aula.html";
-      }
-    }
+    console.log("✅ Usuario conectado:", user.email);
+  } else {
+    console.log("❌ No hay usuario conectado");
   }
 });
