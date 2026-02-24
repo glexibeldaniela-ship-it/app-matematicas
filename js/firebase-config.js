34c56fdf67c73e2a36e8e9eb4b6f842c60b92b51
glexibeldaniela-ship-it <glexibeldaniela@gmail.com>
2026-02-23 21:48:52 -0400

    Actualizar firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,22 +1,19 @@
-// 🔥 Firebase Configuración Modular
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// ⚠️ Pega aquí tu configuración real
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyCqSVoNR5y3kXb-Aihf1UwmvTwo1T0xm_8",
+  authDomain: "mateedupro2026-897fa.firebaseapp.com",
+  projectId: "mateedupro2026-897fa",
+  storageBucket: "mateedupro2026-897fa.firebasestorage.app",
+  messagingSenderId: "242144964491",
+  appId: "1:242144964491:web:04eb900f523a91ef66498e"
 };
 
-// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 
-// Exportar servicios
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+console.log("🔥 Firebase conectado correctamente");
